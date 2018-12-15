73508de5a6e2db025daf9d4e64d29eed0cb49784
William Mckee <hammersmake@gmail.com>
2018-12-15 16:54:13 +1100

    first commit

---
+++ foodgiving/flasktakehome.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import takehomeleft
+
+
+# In[ ]:
+
+
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/createdb')
+def createdb():
+    nameofdb = request.args.get('nameofdb')
+    #lenth = request.args.get('length')
+    #ic.createdbtext(nameofdb)
+    takehomeleft.createFoodDB(nameofdb)
+    takehomeleft.createUserDB(nameofdb)
+    takehomeleft.createReviewDB(nameofdb)
+
+    return('{} database created!'.format(nameofdb))
+
+@app.route('/createuser', methods=['GET', 'POST'])
+def createuser():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        image = request.form.get('image')
+        location = request.form.get('location')
+
+        takehomeleft.createUser(email, password, image, name, location)
+        return('created user {}'.format(email))
+    return(render_template('user.html'))
+
+@app.route('/createfood', methods=['GET', 'POST'])
+def createfood():
+    if request.method == 'POST':
+        #name, image, datecooked, expiredate,
+                 #ingredients, location, myid)
+
+        name = request.form.get('name')
+        image = request.form.get('image')
+        datecooked = request.form.get('datecooked')
+        expiredate = request.form.get('expiredate')
+        ingredients = request.form.get('ingredients')
+        location = request.form.get('location')
+        myid = request.form.get('myid')
+
+        takehomeleft.makeFoodEntry(name, image, datecooked, 
+                                   expiredate, ingredients, location, myid)
+        return('food created')
+    return(render_template('createfood.html'))
+
+#takehomeleft.createReview(1, 2, 1, 5, 'good foods')
+@app.route('/createreview', methods=['GET', 'POST'])
+def createreview():
+    if request.method == 'POST':
+        myid = request.form.get('myid')
+        forid = request.form.get('forid')
+        idfood = request.form.get('idfood')
+        rating = request.form.get('rating')
+        comments = request.form.get('comments')
+        #def createReview(myid, forid, idfood, rating, comments):
+
+        takehomeleft.createReview(myid, forid, idfood, rating, comments)
+        return('review created')
+    return(render_template('review.html'))
+    
+@app.route('/returnfood', methods=['GET'])
+def returnfood():
+    return(str(takehomeleft.returnFoods()))
+
+@app.route('/returnusers', methods=['GET'])
+def returnUsers():
+    return(str(takehomeleft.returnUsers()))
+
+@app.route('/returnreviews', methods=['GET'])
+def returnReviews():
+    return(str(takehomeleft.returnReviews()))
+    
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5556) 
+
+
+# In[ ]:
+
+
+makeFoodEntry(name, image, datecooked, expiredate,
+                 ingredients, location)
+
+
+# In[7]:
+
+
+#takehomeleft.createFoodDB('foodentry')
+
+
+# In[8]:
+
+
+#takehomeleft.createUserDB('foodentry')
+
+
+# In[9]:
+
+
+#takehomeleft.createReviewDB('foodentry')
+
+
+# In[16]:
+
+
+
+#takehomeleft.createUser('joe@me.com', 'test123', 'me.com/joe.png', 'Joe Mckee', '-33.865143, 151.209900')
+
+
+# In[17]:
+
+
+#takehomeleft.makeFoodEntry('chick con cane', 'me.com/pasta.png', '04-12-1999', '06-12-1999', 'chicken', '-33.865143, 151.209900')
+
+
+# In[ ]:
+
+
+
+
+
+# In[18]:
+
+
+#takehomeleft.createReview(1, 2, 1, 5, 'good foods')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[19]:
+
+
+#takehomeleft.returnFoods()
+
+
+# In[20]:
+
+
+#takehomeleft.filterFoods(1)
+
+
+# In[21]:
+
+
+#takehomeleft.returnFoods()
+
+
+# In[25]:
+
+
+takehomeleft.returnUsers()
+
+
+# In[ ]:
+
+
+
+
+
+# In[23]:
+
+
+#takehomeleft.returnReviews()
+
+
+# In[ ]:
+
+
+
+
---
+++ foodgiving/takehomeleft.py
@@ -0,0 +1,355 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Take Home Left
+# 
+# controller for food leftovers. python functions.
+
+# In[54]:
+
+
+import sqlite3
+import arrow
+
+
+# In[66]:
+
+
+def createReviewDB(namedb):
+    conn = sqlite3.connect('{}.db'.format(namedb))
+    c = conn.cursor()
+
+    # Create table
+    c.execute('''CREATE TABLE reviews (myid real, forid real, foodid real, rating real, comments text, datetime text)''')
+
+    
+
+
+# In[70]:
+
+
+def createReview(myid, forid, idfood, rating, comments):
+    timnow = arrow.utcnow()
+    
+    #str(timnow.datetime)
+    
+    conn = sqlite3.connect('foodentry.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO reviews VALUES ('{}', '{}', '{}', '{}', '{}', '{}')".format(myid, forid, idfood, rating, 
+                                                                              comments, 
+                                                                              str(timnow.datetime)))
+    
+    conn.commit()
+
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    conn.close()
+
+
+# In[75]:
+
+
+def lookupReviews():
+    conn = sqlite3.connect('foodentry.db')
+    c = conn.cursor()
+    allfod = c.execute('SELECT * FROM reviews')
+    return(allfod.fetchall())
+    
+    
+
+
+# In[77]:
+
+
+def lookupMyReviews(myid):
+    conn = sqlite3.connect('foodentry.db')
+    c = conn.cursor()
+    allfod = c.execute('SELECT * FROM reviews WHERE myid OR forid = {}'.format(myid))
+    return(allfod.fetchall())
+
+
+# In[78]:
+
+
+#LookupMyReviews(1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[71]:
+
+
+#createreviewdb('foodentry')
+
+
+# In[72]:
+
+
+#createreview(1, 2, 5, 'super good food')
+
+
+# In[76]:
+
+
+#loopupreviews()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+def createUserDB(namedb):
+    conn = sqlite3.connect('{}.db'.format(namedb))
+    c = conn.cursor()
+
+    # Create table
+    c.execute('''CREATE TABLE users
+             (email text, password text, userid text, image text, name text, location text)''')
+
+    
+
+
+# In[ ]:
+
+
+def createUser(email, password, image, name, location):
+    conn = sqlite3.connect('foodentry.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO users VALUES ('{}', '{}', '{}', '{}', '{}', '{}')".format(name, 
+                                                                              password, returnCountUsers() + 1, image, 
+                                                                              name, 
+                                                                              location))
+    
+    conn.commit()
+
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    conn.close()
+    returnCountUsers()
+
+
+# In[35]:
+
+
+def createFoodDB(namedb):
+    conn = sqlite3.connect('{}.db'.format(namedb))
+    c = conn.cursor()
+
+    # Create table
+    c.execute('''CREATE TABLE foods
+             (name text, foodid real, image text, datecook text, dateexpire text, 
+             ingredients text, location text, myid real)''')
+
+    
+
+
+# In[36]:
+
+
+def makeFoodEntry(name, image, datecooked, expiredate,
+                 ingredients, location, myid):
+    conn = sqlite3.connect('foodentry.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO foods VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(name, 
+                                                                              returnCountFood() + 1, image, 
+                                                                              datecooked, 
+                                                                              expiredate, ingredients, location, myid))
+    
+    conn.commit()
+
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    conn.close()
+
+    return({'name' : name, 'foodid' : returnCountFood() + 1, 'image' : image, 'datecooked' : datecooked,
+           'expiredate' : expiredate, 'ingredients' : ingredients, 'location' : location, 'myid' : myid})
+
+
+# In[37]:
+
+
+def returnFoods():
+    conn = sqlite3.connect('foodentry.db')
+    c = conn.cursor()
+    allfod = c.execute('SELECT * FROM foods')
+    return(allfod.fetchall())
+    
+
+
+# In[38]:
+
+
+def returnCountFood():
+    conn = sqlite3.connect('foodentry.db')
+    c = conn.cursor()
+    allfod = c.execute('SELECT count(*) FROM foods')
+    return(allfod.fetchall()[0][0])
+    
+
+
+# In[82]:
+
+
+def returnCountUsers():
+    conn = sqlite3.connect('foodentry.db')
+    c = conn.cursor()
+    allfod = c.execute('SELECT count(*) FROM users')
+    return(allfod.fetchall()[0][0])
+
+
+# In[81]:
+
+
+#returncountusers()
+
+
+# In[83]:
+
+
+def returnUsers():
+    conn = sqlite3.connect('foodentry.db')
+    c = conn.cursor()
+    allfod = c.execute('SELECT * FROM users')
+    return(allfod.fetchall())
+    
+
+
+# In[ ]:
+
+
+def returnReviews():
+    conn = sqlite3.connect('foodentry.db')
+    c = conn.cursor()
+    allfod = c.execute('SELECT * FROM reviews')
+    return(allfod.fetchall())
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[46]:
+
+
+#returncountfood()
+
+
+# In[49]:
+
+
+def filterFoods(searchfood):
+    conn = sqlite3.connect('foodentry.db')
+    c = conn.cursor()
+    allfod = c.execute('SELECT * FROM foods WHERE foodid = {}'.format(searchfood))
+    return(allfod.fetchall())
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[52]:
+
+
+#filterfoods(2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[40]:
+
+
+def makeFoodRequest(whatfood):
+    return('you requested {}'.format(whatfood))
+
+
+# In[41]:
+
+
+#createfoodb('foodentry')
+
+
+# In[44]:
+
+
+#makefoodentry('pasta', 'me.com/img.png', '04/12/1999', '06/12/1999',
+#                 'pork, pasta')
+
+
+# In[45]:
+
+
+#returnfoods()
+
+
+# donatefood/
+# 
+# food info:
+# 
+# type: 
+# 
+# classification: 
+# 
+# quantity
+# 
+# expiration
+# 
+# description
+# 
+# Donate details: 
+# 
+# name:
+# 
+# rating: 
+# 
+# location:
+# 
+# pickup detail:
+# 
+# pickupby:
+# 
+# details:
+# 
+# pickfood:
+# 
+# foodtype: (type of food, ie fruit, vege or meat)
+# 
+# further classification apple, cabbage, meat.
+# 
+# all food.
+# 
+# enter location:
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
